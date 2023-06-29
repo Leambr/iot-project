81cd60ca13c1422939de61c3db5fe73365ad9dd4
Léa M <mhoumadi.lea@gmail.com>
2023-06-29 15:39:18 +0200

    refactor: update dump

--- dump/fitness-park-iot.sql
+++ dump/fitness-park-iot.sql
@@ -7,10 +7,25 @@ CREATE TABLE IF NOT EXISTS `users` (
 CREATE TABLE IF NOT EXISTS `automation` (
 	`id` INT AUTO_INCREMENT PRIMARY KEY,
 	`task_date` DATETIME,
-	`type` ENUM('Lumière', 'Climatisation', 'Chauffage')
+	`type` ENUM('Lumière', 'Climatisation', 'Chauffage'),
+	`is_done` BOOLEAN,
+	`sensor_id` INT,
+	CONSTRAINT fk_sensor_automation FOREIGN KEY (sensor_id) REFERENCES sensor(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS `training_courses` (
+CREATE TABLE IF NOT EXISTS `sensor` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`type` ENUM(
+		'Lumière',
+		'Température',
+		'Ventilation',
+		'Mouvement',
+		'Humidité',
+		'Co2'
+	),
+	`room_id` INT,
+	CONSTRAINT fk_sensor_room FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE ON UPDATE CASCADE
+) CREATE TABLE IF NOT EXISTS `training_courses` (
 	`id` INT AUTO_INCREMENT PRIMARY KEY,
 	`day` ENUM(
 		'Lundi',
@@ -34,7 +49,8 @@ CREATE TABLE IF NOT EXISTS `training_courses` (
 	`coach_name` VARCHAR(255),
 	`is_canceled` BOOLEAN,
 	`is_completed` BOOLEAN,
-	`room_id` INT
+	`room_id` INT,
+	CONSTRAINT fk_training_room FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `rooms` (
@@ -50,47 +66,42 @@ CREATE TABLE IF NOT EXISTS `rooms` (
 );
 
 CREATE TABLE IF NOT EXISTS `light_sensor` (
-	`data_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`sensor_id` INT,
 	`luminosity` INT,
 	`ir` INT,
 	`date` DATETIME,
-	`sensor_id` INT,
-	`room_id` INT,
-	CONSTRAINT fk_light_sensor_room FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT fk_light_sensor FOREIGN KEY (sensor_id) REFERENCES sensor(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `moving_sensor` (
-	`data_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`sensor_id` INT,
 	`is_moving` BOOLEAN,
 	`date` DATETIME,
-	`sensor_id` INT,
-	`room_id` INT,
-	CONSTRAINT fk_moving_sensor_room FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT fk_moving_sensor FOREIGN KEY (sensor_id) REFERENCES sensor(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `temperature_sensor` (
-	`data_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`sensor_id` INT,
 	`temperature` FLOAT,
 	`date` DATETIME,
-	`sensor_id` INT,
-	`room_id` INT,
-	CONSTRAINT fk_temperature_sensor_room FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT fk_temperature_sensor FOREIGN KEY (sensor_id) REFERENCES sensor(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `humidity_sensor` (
-	`data_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`sensor_id` INT,
 	`humidity` FLOAT,
 	`date` DATETIME,
-	`sensor_id` INT,
-	`room_id` INT,
-	CONSTRAINT fk_humidity_sensor_room FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT fk_humidity_sensor FOREIGN KEY (sensor_id) REFERENCES sensor(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `co2_sensor` (
-	`data_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`sensor_id` INT,
 	`co2` INT,
 	`date` DATETIME,
-	`sensor_id` INT,
-	`room_id` INT,
-	CONSTRAINT fk_co2_sensor_room FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT fk_co2_sensor FOREIGN KEY (sensor_id) REFERENCES sensor(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
