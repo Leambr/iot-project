1601453986f1f5ff6d29844f11cacdec84a7e25f
DylanLangevin <dylan.lgvn@gmail.com>
2023-06-29 11:12:54 +0200

    feat room MVC

---
+++ server/src/controllers/RoomController.js
@@ -0,0 +1,28 @@
+const roomService = require("../services/RoomService");
+const responseFormatter = require("../utils/responseFormatter");
+
+const roomController = {
+
+    getAllRooms: async (_, res) => {
+
+        try {
+            const rooms = await roomService.getAllRooms();
+            const response = responseFormatter.formatResponse(res.statusCode, rooms);
+            res.json(response);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+
+    getRoomById: async (req, res) => {
+        try {
+            const room = await roomService.getRoomById(req.params.id);
+            const response = responseFormatter.formatResponse(res.statusCode, room);
+            res.json(response);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+}
+
+module.exports = roomController;
