44f7bf408d62fbd7ae6cfaff0b3a2aa9755071cc
Louis <louisrca@MacBook-Pro-de-Louis.local>
2023-07-18 14:54:41 +0200

    feat: automation calendar page

--- frontend/src/components/calendar/calendarComponents/CalendarNavigation.tsx
+++ frontend/src/components/calendar/calendarComponents/CalendarNavigation.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import s from '../Calendar.module.css';
 import { useEffect, useState } from 'react';
 import Paper from '@mui/material/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
+import { AppointmentModel } from '@devexpress/dx-react-scheduler';
 import {
     Scheduler,
     MonthView,
@@ -12,24 +14,22 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 export const CalendarNavigation = () => {
-    const appointments = [
+    const appointments: AppointmentModel[] = [
         {
-            title: 'Website Re-Design Plan',
-            startDate: new Date(2023, 7, 23, 9, 30),
-            endDate: new Date(2023, 7, 23, 11, 30),
-            id: 0,
-            location: 'Room 1',
+            startDate: '2023-07-20T09:45',
+            endDate: '2023-07-22T11:00',
+            title: 'Yoga',
         },
         {
-            title: 'Book Flights to San Fran for Sales Trip',
-            startDate: new Date(2023, 7, 24, 9, 30),
-            endDate: new Date(2023, 7, 24, 11, 30),
-            id: 1,
-            location: 'Room 2',
+            startDate: '2023-07-18T12:00',
+            endDate: '2023-07-18T13:30',
+            title: 'Abdos/Fessiers',
         },
     ];
+
+    console.log(appointments);
     return (
-        <Paper>
+        <Paper className={s.calendarScheduler}>
             <Scheduler data={appointments}>
                 <ViewState />
                 <MonthView />
