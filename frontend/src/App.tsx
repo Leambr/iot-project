72f4c92be13b7cde702f4c2761cd2a5b84d715cb
Léa M <mhoumadi.lea@gmail.com>
2023-07-18 15:38:34 +0200

    refactor: add login route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import s from './App.module.css';
-import { Routes, Route } from 'react-router-dom';
-import { Dashboard } from './components/dashboard/Dashboard';
-import { Navbar } from './components/navbar/Navbar';
-import { BackgroundWrapper } from './components/backgroundWrapper/BackgroundWrapper';
-import Login from './components/login/Login';
 import { Calendar } from './components/calendar/Calendar';
+import { Dashboard } from './components/dashboard/Dashboard';
+import Login from './components/login/Login';
 import { PieChart } from './components/pieChart/PieChart';
 
 function App() {
     return (
         <div className={s.appWrapper}>
-            <Login />
             <Routes>
+                <Route path="/login" element={<Login />} />
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/calendar" element={<Calendar />} />
                 <Route path="/piechart" element={<PieChart />} />
