2df17e6e09fd90d6fdc0e04287a4545fbb09c2c7
Louis <louisrca@MacBook-Pro-de-Louis.local>
2023-07-19 21:08:59 +0200

    feat: calendar page

--- frontend/src/components/calendar/calendarComponents/CalendarNavigation.tsx
+++ frontend/src/components/calendar/calendarComponents/CalendarNavigation.tsx
@@ -8,6 +8,7 @@ import {
     Toolbar,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import Paper from '@mui/material/Paper';
+import { useState } from 'react';
 import s from '../Calendar.module.css';
 
 export const CalendarNavigation = () => {
@@ -24,17 +25,56 @@ export const CalendarNavigation = () => {
         },
     ];
 
+    const [selectedAppointment, setSelectedAppointment] = useState<AppointmentModel | null>(null);
+
+    const handleAppointmentClick = (appointment: AppointmentModel) => {
+        setSelectedAppointment(appointment);
+    };
+
     console.log(appointments);
     return (
         <Paper className={s.calendarScheduler}>
-            <Scheduler data={appointments}>
+            <Scheduler height={580} data={appointments}>
                 <ViewState />
                 <MonthView />
                 <Toolbar />
                 <DateNavigator />
                 <TodayButton />
-                <Appointments />
+
+                <Appointments
+                    appointmentComponent={(props) => (
+                        <Appointments.Appointment {...props}>
+                            <div
+                                className={s.open}
+                                onClick={() => handleAppointmentClick(props.data)}
+                            >
+                                {props.children}
+                            </div>
+                        </Appointments.Appointment>
+                    )}
+                />
             </Scheduler>
+            {/* Modale */}
+            {selectedAppointment && (
+                <div className={s.modal}>
+                    <div className={s.modalContent}>
+                        <span className={s.close} onClick={() => setSelectedAppointment(null)}>
+                            &times;
+                        </span>
+                        <h2>{selectedAppointment.title}</h2>
+                        <div className={s.dateInformation}>
+                            <p>
+                                Début de l'automatisation :{' '}
+                                {selectedAppointment.startDate?.toString()}
+                            </p>
+
+                            <p>
+                                Fin de l'automatisation : {selectedAppointment.endDate?.toString()}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            )}
         </Paper>
     );
 };
